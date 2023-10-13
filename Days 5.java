e693d8b6afd0490828a73cf6d048129b4d3cb24c
Musdalifa1359 <100498456+Musdalifa1359@users.noreply.github.com>
2023-10-13 13:27:55 +0800

    Create Days 5.java

---
+++ Days 5.java
@@ -0,0 +1,18 @@
+
+package perulangan;
+
+
+public class Perulangan {
+
+    public static void main(String[] argument) {
+               for(int i = 3; i <= 38; i += 3){
+            System.out.println(i + "");
+        }
+       
+    }
+    
+}
+
+    
+    
+
