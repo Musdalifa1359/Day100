e88a2ba668565a736361e63ef067883dc63af80a
Musdalifa1359 <100498456+Musdalifa1359@users.noreply.github.com>
2023-10-14 21:36:35 +0800

    Create days_6.java

---
+++ days_6.java
@@ -0,0 +1,25 @@
+package tipedata_days6;
+
+
+public class Tipedata_days6 {
+
+    
+    public static void main(String[] args) {
+        String nama = "musdalifa";
+        int umur = 20;
+        double tinggi = 157;
+        char jenisKelamin = 'p'; 
+        float berat = 45;
+        boolean perilaku = false;
+        
+          
+        
+        System.out.println("nama " +nama);
+        System.out.println("umur : " + umur);
+        System.out.println("tinggi :" +tinggi);
+        System.out.println("jenis kelamin: " +jenisKelamin);
+        System.out.println("berat : "+berat);
+        System.out.println("perilaku :"+perilaku);
+    }
+    
+}
