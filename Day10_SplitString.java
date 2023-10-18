b52e6015a8edf01767586fbd7602373abbaef4bb
Musdalifa1359 <100498456+Musdalifa1359@users.noreply.github.com>
2023-10-18 18:52:08 +0700

    Create Day10_SplitString.java

---
+++ Day10_SplitString.java
@@ -0,0 +1,15 @@
+package com.mycompany.days10_splitstringg;
+
+public class Days10_Splitstringg {
+
+    public static void main(String[] args) {
+   
+        String inputString = "semangat insyaaAllah bisa lulus PBO";
+        String[] ifa = inputString.split(" ");
+        for (String word : ifa) {
+            System.out.println(word);
+    
+        }
+            
+        }
+    }
