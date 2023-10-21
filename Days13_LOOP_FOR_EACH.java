e55fb40c2876b3591a32e2b37f74cbbcf9d81ff5
Musdalifa1359 <100498456+Musdalifa1359@users.noreply.github.com>
2023-10-21 19:58:52 +0700

    Create Days13_LOOP_FOR_EACH.java

---
+++ Days13_LOOP_FOR_EACH.java
@@ -0,0 +1,24 @@
+
+package com.mycompany.days13_loopforeach;
+
+
+public class Days13_LoopForEach {
+
+    public static void main(String[] args) {
+       
+        String[]nama = {"ifa", "dini", "faras"};
+        for(String item: nama){
+            System.out.println("nomor:" + item);  
+        }
+        
+    }
+}
+                 
+   
+    
+
+ 
+
+
+    
+
