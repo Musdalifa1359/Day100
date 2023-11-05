204c4d3db8d7f66f5e6371c28071be8960350af2
Musdalifa1359 <100498456+Musdalifa1359@users.noreply.github.com>
2023-11-05 20:00:20 +0700

    Create Days28_MenghitungNilai_TipeData_Long.java

---
+++ Days28_MenghitungNilai_TipeData_Long.java
@@ -0,0 +1,26 @@
+
+package com.mycompany.days28_tipedatalong;
+
+public class Days28_TipeDatalong {
+
+    public static void main(String[] args) {
+       
+        long angka1 = 340L;
+        long angka2 = 23L;
+        
+        long hasilPenjumlahan = angka1 + angka2;
+        System.out.println("Hasil Penjumlahan: " + hasilPenjumlahan);
+
+        long hasilPengurangan = angka1 - angka2;
+        System.out.println("Hasil Pengurangan: " + hasilPengurangan);
+
+        long hasilPerkalian = angka1 * angka2;
+        System.out.println("Hasil Perkalian: " + hasilPerkalian);
+
+        long hasilPembagian = angka1 / angka2;
+        System.out.println("Hasil Pembagian: " + hasilPembagian);
+    }
+}
+
+    
+
