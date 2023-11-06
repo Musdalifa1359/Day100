7a0d06ad10ae1d62dc4d3a961a2c1e9efb8caecc
Musdalifa1359 <100498456+Musdalifa1359@users.noreply.github.com>
2023-11-06 16:35:50 +0700

    Create Days29_TipeDataPrimitif_float.java

---
+++ Days29_TipeDataPrimitif_float.java
@@ -0,0 +1,23 @@
+package com.mycompany.days29_tipedatafloat;
+
+
+public class Days29_TipeDataFloat {
+
+    public static void main(String[] args) {
+    
+        float angkaA = 12.8f;
+        float angkaB = 7.90f;
+        float hasilPenjumlahan = angkaA + angkaB;        
+        float hasilPengurangan = angkaA - angkaB;
+        float hasilPerkalian = angkaA * angkaB;        
+        float hasilPembagian = angkaA / angkaB;
+
+        System.out.println("Hasil Penjumlahan: " + hasilPenjumlahan);
+        System.out.println("Hasil Pengurangan: " + hasilPengurangan);
+        System.out.println("Hasil Perkalian: " + hasilPerkalian);
+        System.out.println("Hasil Pembagian: " + hasilPembagian);
+    }
+}
+
+    
+
