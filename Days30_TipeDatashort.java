a573e65662ce2913bbd8c792e9d769cb2cd4a132
Musdalifa1359 <100498456+Musdalifa1359@users.noreply.github.com>
2023-11-07 19:44:19 +0700

    Create Days30_TipeDatashort.java

---
+++ Days30_TipeDatashort.java
@@ -0,0 +1,17 @@
+package com.mycompany.days29_tipedatafloat;
+
+public class Days30_TipeDataShort {
+    public static void main(String[] args) {
+        short nilaiShort = 12345;
+
+        System.out.println("Nilai short: " + nilaiShort);
+        short hasilPenambahan = (short) (nilaiShort + 125);
+        short hasilPengurangan = (short) (nilaiShort - 60);
+        short hasilPerkalian = (short) (nilaiShort * 2);
+        System.out.println("Hasil Penambahan: " + hasilPenambahan);
+        System.out.println("Hasil Pengurangan: " + hasilPengurangan);
+        System.out.println("Hasil Perkalian: " + hasilPerkalian);
+    }
+}
+  
+
