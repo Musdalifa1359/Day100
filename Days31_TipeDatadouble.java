9e3091bfec188d564787db971f576db5e2c5c16b
Musdalifa1359 <100498456+Musdalifa1359@users.noreply.github.com>
2023-11-08 18:42:12 +0700

    Create Days31_TipeDatadouble.java

---
+++ Days31_TipeDatadouble.java
@@ -0,0 +1,21 @@
+package com.mycompany.days31_tipedatadouble;
+
+public class Days31_TipeDatadouble {
+
+    public static void main(String[] args) {
+        double nilaiDouble1 = 32.8;
+        double nilaiDouble2 = 7.980;
+        double hasilPenjumlahan = nilaiDouble1 + nilaiDouble2;
+        double hasilPengurangan = nilaiDouble1 - nilaiDouble2;
+        double hasilPerkalian = nilaiDouble1 * nilaiDouble2;
+        double hasilPembagian = nilaiDouble1 / nilaiDouble2;
+      
+        System.out.println("Hasil Penjumlahan: " + hasilPenjumlahan);
+        System.out.println("Hasil Pengurangan: " + hasilPengurangan);
+        System.out.println("Hasil Perkalian: " + hasilPerkalian);
+        System.out.println("Hasil Pembagian: " + hasilPembagian);
+    }
+}
+
+
+
