36a5225eccce2237fdcdeed190279b791b06b677
Musdalifa1359 <100498456+Musdalifa1359@users.noreply.github.com>
2023-11-09 14:22:54 +0700

    Create Days32_Operasi_typedataprimitif_int.java

---
+++ Days32_Operasi_typedataprimitif_int.java
@@ -0,0 +1,27 @@
+
+package com.mycompany.days32_operasitypedataint;
+
+public class Days32_OperasiTypeDataInt {
+
+    public static void main(String[] args) {
+
+        int angkaA = 67;
+        int angkaB = 13;
+        int hasilPenambahan = angkaA + angkaB;
+        System.out.println("Hasil Penambahan: " + hasilPenambahan);
+
+        int hasilPengurangan = angkaA - angkaB;
+        System.out.println("Hasil Pengurangan: " + hasilPengurangan);
+
+        int hasilPerkalian = angkaA * angkaB;
+        System.out.println("Hasil Perkalian: " + hasilPerkalian);
+
+        int hasilPembagian = angkaA / angkaB;
+        System.out.println("Hasil Pembagian: " + hasilPembagian);
+    }
+}
+
+
+
+    
+
