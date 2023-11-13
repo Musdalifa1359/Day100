0805130faf25ce8cbae792dae82abcca97fe9749
Musdalifa1359 <100498456+Musdalifa1359@users.noreply.github.com>
2023-11-13 17:53:07 +0800

    Create Days36_TipeDataPrimitif_char.java

---
+++ Days36_TipeDataPrimitif_char.java
@@ -0,0 +1,15 @@
+package com.mycompany.days36_tipedatachar;
+
+public class Days36_TipeDataChar {
+
+    public static void main(String[] args) {
+
+        char myChar = 'B';
+        
+        System.out.println("Nilai char: " + myChar);
+        
+        char nextChar = (char) (myChar + 2);
+        System.out.println("Char setelah ditambah 2: " + nextChar);
+    }
+}
+
