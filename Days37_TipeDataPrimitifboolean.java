5a1e98df5305f4cdef01ce59079ddd9b0e9b8552
Musdalifa1359 <100498456+Musdalifa1359@users.noreply.github.com>
2023-11-14 14:48:33 +0800

    Create Days37_TipeDataPrimitifboolean.java

---
+++ Days37_TipeDataPrimitifboolean.java
@@ -0,0 +1,19 @@
+package com.mycompany.days37_tipedataprimitifboolean;
+
+public class Days37_TipeDataPrimitifboolean {
+
+    public static void main(String[] args) {
+
+        boolean isTrue = true;
+        boolean isFalse = false;
+        System.out.println("isTrue: " + isTrue);
+        System.out.println("isFalse: " + isFalse);
+        if (isTrue) {
+            System.out.println("Ini akan ditampilkan karena isTrue bernilai true.");
+        } else {
+            System.out.println("Ini tidak akan ditampilkan.");
+        }
+    }
+}
+
+
