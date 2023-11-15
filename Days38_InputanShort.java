52773e4b26a38bbc44258f628426c4a88dfe56b5
Musdalifa1359 <100498456+Musdalifa1359@users.noreply.github.com>
2023-11-15 17:40:38 +0700

    Create Days38_InputanShort.java

---
+++ Days38_InputanShort.java
@@ -0,0 +1,19 @@
+package com.mycompany.days38_inputanshort;
+
+import java.util.Scanner;
+
+public class Days38_InputanShort {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Memasukkan nilai short: ");
+
+        short nilaiShort = scanner.nextShort();
+
+        System.out.println("Nilai short yang ifa masukkan: " + nilaiShort);
+
+    }
+}
+
