8ac3ad00659c17928cf1dae7da7579d29a070bed
Musdalifa1359 <100498456+Musdalifa1359@users.noreply.github.com>
2023-11-17 21:25:37 +0700

    Create Days40_PerulanganDenganTipeDataI_int.java

---
+++ Days40_PerulanganDenganTipeDataI_int.java
@@ -0,0 +1,23 @@
+
+package com.mycompany.days40_perulangandengentipedataint;
+
+public class Days40_PerulanganDengenTipeDataint {
+
+    public static void main(String[] args) {
+        for (int A = 1; A <= 10; A++) {
+            System.out.println("Nilai A: " + A);
+        }
+
+        int B = 1;
+        while (B <= 10) {
+            System.out.println("Nilai B: " + B);
+            B++;
+        }
+        int C = 1;
+        do {
+            System.out.println("Nilai C: " + C);
+            C++;
+        } while (C <= 10);
+    }
+}
+
