bf2f237d14b3c5932de716a7ea75735abca7a5e6
Musdalifa1359 <100498456+Musdalifa1359@users.noreply.github.com>
2023-11-26 12:23:28 +0700

    Create Days49_MenghitungStokBarang.java

---
+++ Days49_MenghitungStokBarang.java
@@ -0,0 +1,21 @@
+package com.mycompany.days49_menghitungstokbarang;
+
+import java.util.Scanner;
+
+public class Days49_MenghitungStokBarang {
+
+    public static void main(String[] args) {
+   
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan jumlah stok awal: ");
+        int stokAwal = scanner.nextInt();
+        System.out.print("Masukkan jumlah barang yang masuk: ");
+        int barangMasuk = scanner.nextInt();
+        System.out.print("Masukkan jumlah barang yang keluar: ");
+        int barangKeluar = scanner.nextInt();
+        int stokAkhir = stokAwal + barangMasuk - barangKeluar;
+        System.out.println("Stok akhir barang: " + stokAkhir);
+
+    }
+}
+
