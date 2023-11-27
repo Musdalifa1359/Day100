ebf31f6a039ac0257813120f675216041509880e
Musdalifa1359 <100498456+Musdalifa1359@users.noreply.github.com>
2023-11-27 10:33:18 +0700

    Create Days50_KonversiNilaiIntegerkeBoolean.java

---
+++ Days50_KonversiNilaiIntegerkeBoolean.java
@@ -0,0 +1,16 @@
+
+package com.mycompany.days50_konversinilaiintegerkeboolean;
+
+public class Days50_KonversiNilaiIntegerKeBoolean {
+
+    public static void main(String[] args) {
+     
+        int nilaiInteger = 23;
+
+        boolean nilaiBoolean = (nilaiInteger != 0);
+
+        System.out.println("Nilai Integer: " + nilaiInteger);
+        System.out.println("Nilai Boolean: " + nilaiBoolean);
+    }
+}
+
