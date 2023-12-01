9fed5cb30f8134819d7bb5b8dd505926f8fa6474
Musdalifa1359 <100498456+Musdalifa1359@users.noreply.github.com>
2023-12-01 21:18:29 +0800

    Create Days54_PerulanganDoWhile.java

---
+++ Days54_PerulanganDoWhile.java
@@ -0,0 +1,14 @@
+package musdalifa54;
+ class Musdalifa54 {
+
+    
+    public static void main(String[] args) {
+        
+        short angka = 55;
+        
+        do{
+            System.out.println(angka);
+        }while ( angka <= 100);
+        
+        
+    }
