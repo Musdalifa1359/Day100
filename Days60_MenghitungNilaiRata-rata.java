a1314200fd32a21a5ad757a9cb7696f857f20ded
Musdalifa1359 <100498456+Musdalifa1359@users.noreply.github.com>
2023-12-07 19:25:44 +0800

    Create Days60_MenghitungNilaiRata-rata.java

---
+++ Days60_MenghitungNilaiRata-rata.java
@@ -0,0 +1,29 @@
+
+package days60_menghitungnilairata.rata;
+
+import java.util.Scanner;
+
+public class Days60_MenghitungNilaiRataRata {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan jumlah bilangan: ");
+        int jumlahBilangan = scanner.nextInt();
+
+        double total = 0;
+
+        for (int i = 0; i < jumlahBilangan; i++) {
+            System.out.print("Masukkan bilangan ke-" + (i + 1) + ": ");
+            double bilangan = scanner.nextDouble();
+
+            total += bilangan;
+        }
+
+        double rataRata = total / jumlahBilangan;
+
+        System.out.println("Rata-rata: " + rataRata);
+    }
+}
+
+
