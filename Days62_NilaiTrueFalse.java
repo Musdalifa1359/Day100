f54e8104f126ce707f9a9c71a8b6baf14e3a6d39
Musdalifa1359 <100498456+Musdalifa1359@users.noreply.github.com>
2023-12-09 16:14:54 +0800

    Create Days62_NilaiTrueFalse.java

---
+++ Days62_NilaiTrueFalse.java
@@ -0,0 +1,13 @@
+package com.mycompany.days62_nilaitruefalse;
+
+public class Days62_NilaiTrueFalse {
+
+    public static void main(String[] args) {
+        int angka = 30;
+        boolean lulus = angka >= 80;
+        
+        System.out.println("Nilai angka: " + angka);
+        System.out.println("Apakah lulus? " + lulus);
+    }
+}
+
