3ea78d032c2a2fff6bdae70042939b8f1eac0ac8
Musdalifa1359 <100498456+Musdalifa1359@users.noreply.github.com>
2023-12-10 19:11:21 +0800

    Create Days63_MembuatBintang.java

---
+++ Days63_MembuatBintang.java
@@ -0,0 +1,18 @@
+
+package days63_membuatsegitiga;
+
+public class Days63_MembuatBintang {
+
+    public static void main(String[] args) {
+        
+        int n = 6;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
