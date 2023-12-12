6d8fe0a65708a4882dd64421ed3b69817d8d72fa
Musdalifa1359 <100498456+Musdalifa1359@users.noreply.github.com>
2023-12-12 19:12:58 +0800

    Create Days65_MenyimpanNamaBarang.java

---
+++ Days65_MenyimpanNamaBarang.java
@@ -0,0 +1,23 @@
+
+package days65_menyimpannamabarang;
+
+
+public class Days65_MenyimpanNamaBarang {
+
+    public static void main(String[] args) {
+      
+        String[] namaBarang = new String[10];
+
+        namaBarang[0] = "kertas";
+        namaBarang[1] = "Hp";
+        namaBarang[2] = "TV";
+        namaBarang[3] = "televon";
+        namaBarang[4] = "pilpen";
+
+        for (int i = 0; i < namaBarang.length; i++) {
+            System.out.println(namaBarang[i]);
+        }
+    }
+}
+
+
