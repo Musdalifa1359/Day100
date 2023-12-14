b62d9b6c365505f4c0889c527ed9456aef2d06c2
Musdalifa1359 <100498456+Musdalifa1359@users.noreply.github.com>
2023-12-14 19:57:05 +0800

    Create Days67_PerbandinganDuaNilaiNumerik.java

---
+++ Days67_PerbandinganDuaNilaiNumerik.java
@@ -0,0 +1,24 @@
+
+package days67_pembagianduanilainumerik;
+
+public class Days67_PembagianDuaNilaiNumerik {
+
+    public static void main(String[] args) {
+    int a = 10;
+    int b = 20;
+
+System.out.println(a == b); 
+
+System.out.println(a != b); 
+
+System.out.println(a < b); 
+
+System.out.println(a > b); 
+
+System.out.println(a <= b); 
+
+System.out.println(a >= b); 
+
+    }
+    
+}
