57d405e823e823181b16d50aba03ea8f1ef615b1
Musdalifa1359 <100498456+Musdalifa1359@users.noreply.github.com>
2023-12-15 19:17:15 +0700

    Create Days68_PercabanganXOR.java

---
+++ Days68_PercabanganXOR.java
@@ -0,0 +1,20 @@
+
+package days68_percabanganxor;
+
+public class Days68_PercabanganXor {
+
+    public static void main(String[] args) {
+       
+        boolean a = true;
+        boolean b = false;
+
+        boolean c = a ^ b;
+
+        if (c) {
+            System.out.println("Nilai XOR adalah true");
+        } else {
+            System.out.println("Nilai XOR adalah false");
+        }
+    }
+}
+
