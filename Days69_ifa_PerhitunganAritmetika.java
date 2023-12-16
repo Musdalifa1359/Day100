eea40195f6b60d8f6882cc32a1909f8aa7671d2e
Musdalifa1359 <100498456+Musdalifa1359@users.noreply.github.com>
2023-12-16 20:32:46 +0700

    Create Days69_ifa_PerhitunganAritmetika.java

---
+++ Days69_ifa_PerhitunganAritmetika.java
@@ -0,0 +1,27 @@
+
+package com.mycompany.days69_perhitunganaritmetika;
+
+public class Days69_PerhitunganAritmetika {
+
+    public static void main(String[] args) {
+      
+        int a = 20;
+        int b = 7;
+
+        int c = a + b;
+        System.out.println("a + b = " + c);
+
+        int d = a - b;
+        System.out.println("a - b = " + d);
+
+        int e = a * b;
+        System.out.println("a * b = " + e);
+
+        int f = a / b;
+        System.out.println("a / b = " + f);
+
+        int g = a % b;
+        System.out.println("a % b = " + g);
+    }
+}
+
