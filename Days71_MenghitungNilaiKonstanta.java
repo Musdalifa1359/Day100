db2def4b91db9766a93156cf696f97ffff4a6294
Musdalifa1359 <100498456+Musdalifa1359@users.noreply.github.com>
2023-12-18 20:19:07 +0800

    Create Days71_MenghitungNilaiKonstanta.java

---
+++ Days71_MenghitungNilaiKonstanta.java
@@ -0,0 +1,18 @@
+
+package days71_menghitungnilaikonstanta;
+
+public class Days71_MenghitungNilaiKonstanta {
+
+    public static void main(String[] args) {
+
+    final double PI = 3.14;
+
+ 
+        double jariJari = 5.0;
+double Lingkaran = PI * jariJari * jariJari;
+        String luasLingkaran = null;
+
+        System.out.println("Luas Lingkaran dengan jari-jari " + jariJari + " adalah: " + luasLingkaran);
+    }
+}
+
